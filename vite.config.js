15ffdeaf7e19429bdf8c3c6d21f24c55e4792c0c
Shubham Sourav <shubhamsourav475@gmail.com>
2026-02-13 20:07:15 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/valentine-proposal/',   // 👈 repo name
 })
